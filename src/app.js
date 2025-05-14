673e13434643928a0afb21f179ad6b181cb74815
developerdesigner18 <developer.designer18@gmail.com>
2025-05-14 12:14:44 +0530

    feat: update package.json and app.js to initialize Express server with nodemon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("App in Express");
+import express from "express";
+
+const PORT = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World! :)");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
