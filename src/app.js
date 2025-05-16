990fee7a560f384459b0296b6b35467a27336627
developerdesigner18 <developer.designer18@gmail.com>
2025-05-16 17:34:03 +0530

    feat: add example environment configuration file with necessary variables for application setup

--- src/app.js
+++ src/app.js
@@ -56,6 +56,9 @@ app.use(passport.session());
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: "./public" });
 });
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./public" });
+});
 
 app.use("/auth", AuthRouter);
 app.use("/api/v1/user", AuthRouter);
